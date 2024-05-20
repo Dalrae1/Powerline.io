e10a3a3e16cb20776be1880a32599969b30d938a
Dalrae1 <52721173+Dalrae1@users.noreply.github.com>
2024-05-20 09:52:05 -0700

    Update login-handler.php

--- webserver/login-handler.php
+++ webserver/login-handler.php
@@ -1,5 +1,5 @@
 <?php
-$json = file_get_contents("testfile.txt");
+$json = file_get_contents("php://input");
 $a = json_decode($json);
 $token = $json;
 $jwt = base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1])));
@@ -11,6 +11,4 @@ $users = json_decode(file_get_contents("users.json"), true);
 $users[$user_info['email']] = $user_info;
 fwrite($usersFile, json_encode($users));
 fclose($usersFile);
-
-
 ?>
