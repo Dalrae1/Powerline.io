5f1255bc53f40e9fc4079a7dd3266a25b81f64e3
Dalrae1 <52721173+Dalrae1@users.noreply.github.com>
2024-06-28 08:00:36 -0700

    Auto restart node process

--- PowerlineServer.bat
+++ PowerlineServer.bat
@@ -1,2 +1,14 @@
+@echo off
+setlocal
+
+:loop
+echo Starting Node.js process...
 node PowerlineServer.js
-pause
+
+if %ERRORLEVEL% NEQ 0 (
+    echo Node.js process crashed with exit code %ERRORLEVEL%. Restarting in 5 seconds...
+    timeout /t 5
+    goto loop
+)
+
+endlocal
